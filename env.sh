a0e0cebf67bbac6804b73c093c86777f3c57803a
Colin Unger <unger@stanford.edu>
2021-03-07 14:36:17 -0800

    Get building on sapling

--- env.sh
+++ env.sh
@@ -1,24 +1,23 @@
 #! /usr/bin/env bash
 
-ml restore flexflow
+#ml restore flexflow
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ENABLE_DISTRIBUTED=0
-
 export HASH=$(md5sum "$DIR/env.sh")
 
-export NCCL_HOME="$HOME/FlexFlow/nccl/build/"
 export FF_HOME="$DIR"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/FlexFlow/protobuf/src/.libs/"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/FlexFlow/nccl/build/lib/"
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/FlexFlow/protobuf/src/.libs/"
 export GASNET_BACKTRACE=1
 export DEBUG=0
-export GASNET="$HOME/FlexFlow/gasnet/release"
-export USE_GASNET=$ENABLE_DISTRIBUTED
-export LD_FLAGS="-lcudnn -lpmix ${LD_FLAGS:-} -L$HOME/FlexFlow/protobuf/src/.libs/ -lcuda -lcurand -lcublas -lprotobuf"
+#export GASNET="$HOME/FlexFlow/gasnet/release"
+#export GASNET=":
+export GASNET="$SPACK_ENV/.spack-env/view/"
+export USE_GASNET=1
+#export LD_FLAGS="-lcudnn -lpmix ${LD_FLAGS:-} -L$HOME/FlexFlow/protobuf/src/.libs/ -lcuda -lcurand -lcublas -lprotobuf"
+# export LD_FLAGS="-lcudnn -lpmix ${LD_FLAGS:-} -lcuda -lcurand -lcublas"
 export USE_CUDA=1
 export USE_HDF=1
-export CMAKE_ARGS="-DProtobuf_INCLUDE_DIR='/home/users/unger/FlexFlow/protobuf/src/' -DONNX_USE_PROTOBUF_SHARED_LIBS=OFF"
-export PATH="$HOME/FlexFlow/protobuf/src/.libs/:$PATH"
-export FF_ENABLE_NCCL=0 #$ENABLE_DISTRIBUTED
+#export CMAKE_ARGS="-DProtobuf_INCLUDE_DIR='/home/users/unger/FlexFlow/protobuf/src/' -DONNX_USE_PROTOBUF_SHARED_LIBS=OFF"
+#export PATH="$HOME/FlexFlow/protobuf/src/.libs/:$PATH"
+export FF_ENABLE_NCCL=1
--- ffcompile.sh
+++ ffcompile.sh
@@ -17,11 +17,11 @@ if [ -z "$APP" ]; then echo "Usage: ./ffcompile app_dir"; exit; fi
 
 if [ -z "$FF_HOME" ]; then echo "FF_HOME variable is not defined, aborting compile"; exit; fi
 
-if [[ ! -f $FF_HOME/protobuf/src/.libs/protoc ]]; then echo "Please build the FlexFlow Protocol Buffer library"; exit; fi
+#if [[ ! -f $FF_HOME/protobuf/src/.libs/protoc ]]; then echo "Please build the FlexFlow Protocol Buffer library"; exit; fi
 
-echo "Use the FlexFlow protoc"
+#echo "Use the FlexFlow protoc"
 #$FF_HOME/protobuf/src/protoc -I=$FF_HOME/src/runtime --cpp_out=$FF_HOME/src/runtime $FF_HOME/src/runtime/strategy.proto
 #$FF_HOME/protobuf/src/.libs/protoc -I=$FF_HOME/src/runtime --cpp_out=$FF_HOME/src/runtime $FF_HOME/src/runtime/strategy.proto
 
 cd $APP
-make -j 12
+make -j 32
