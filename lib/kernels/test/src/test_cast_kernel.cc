2e2ae131b3d2fa1a11278e5e3482ceedd47f780c
Dylan Lim <dylaneverettlim@gmail.com>
2024-07-12 12:54:48 -0700

    test utils modification, cast, reverse, and replicate cpu kernels

--- lib/kernels/test/src/test_cast_kernel.cc
+++ lib/kernels/test/src/test_cast_kernel.cc
@@ -75,4 +75,60 @@ TEST_SUITE(FF_TEST_SUITE) {
       CHECK(accessors_are_equal(output_accessor_gpu, output_accessor_cpu));
     }
   }
+
+  TEST_CASE("Check Cast Forward Kernel against CPU Kernel") {
+    ManagedFFStream managed_stream{};
+
+    Allocator gpu_allocator = create_local_cuda_memory_allocator();
+    Allocator cpu_allocator = create_local_cpu_memory_allocator();
+
+    TensorShape input_shape =
+        make_tensor_shape_from_legion_dims<DataType::FLOAT>({100, 100});
+    TensorShape output_shape =
+        make_tensor_shape_from_legion_dims<DataType::INT32>({100, 100});
+
+    GenericTensorAccessorW output_accessor_gpu =
+        gpu_allocator.allocate_tensor(output_shape);
+    GenericTensorAccessorW output_accessor_cpu =
+        cpu_allocator.allocate_tensor(output_shape);
+
+    // Only calling forward kernel as backward kernel is exactly the same
+    SUBCASE("forward_kernel") {
+      auto transform = [start_val = 1.1f,
+                        counter = 0.0f](float input) mutable -> float {
+        return start_val + counter++;
+      };
+
+      // Run GPU Forward Kernel
+      GenericTensorAccessorW input_accessor_gpu =
+          create_transformed_accessor_w<float, float>(
+              input_shape, gpu_allocator, transform, false);
+      Kernels::Cast::forward_kernel(
+          managed_stream.raw_stream(),
+          read_only_accessor_from_write_accessor(input_accessor_gpu),
+          output_accessor_gpu,
+          DataType::FLOAT,
+          DataType::INT32);
+      std::vector<int32_t> result_data_gpu =
+          load_accessor_data<DataType::INT32>(
+              read_only_accessor_from_write_accessor(output_accessor_gpu),
+              true);
+
+      // Run CPU Forward Kernel
+      GenericTensorAccessorW input_accessor_cpu =
+          create_transformed_accessor_w<float, float>(
+              input_shape, cpu_allocator, transform, true);
+      Kernels::Cast::CPU::forward_kernel(
+          read_only_accessor_from_write_accessor(input_accessor_cpu),
+          output_accessor_cpu,
+          DataType::FLOAT,
+          DataType::INT32);
+      std::vector<int32_t> result_data_cpu =
+          load_accessor_data<DataType::INT32>(
+              read_only_accessor_from_write_accessor(output_accessor_cpu),
+              false);
+
+      CHECK(result_data_gpu == result_data_cpu);
+    }
+  }
 }
--- lib/kernels/test/src/test_reverse_kernels.cc
+++ lib/kernels/test/src/test_reverse_kernels.cc
@@ -137,4 +137,109 @@ TEST_SUITE(FF_TEST_SUITE) {
                                 input_grad_accessor_cpu));
     }
   }
+
+  TEST_CASE("Check Reverse Forward and Backward Kernels against CPU Kernels") {
+    std::size_t num_out_blks = 2;
+    std::size_t reverse_dim_size = 3;
+    std::size_t in_blk_size = 5;
+
+    TensorShape input_shape =
+        make_tensor_shape_from_legion_dims<DataType::FLOAT>(
+            {num_out_blks, reverse_dim_size, in_blk_size});
+    TensorShape output_shape = input_shape;
+
+    ManagedPerDeviceFFHandle managed_handle{};
+    ManagedFFStream managed_stream{};
+
+    Allocator gpu_allocator = create_local_cuda_memory_allocator();
+    Allocator cpu_allocator = create_local_cpu_memory_allocator();
+
+    SUBCASE("forward_kernel") {
+      auto transform = [counter = 0.0f](float val) mutable {
+        return counter++;
+      };
+
+      // Run GPU Cast Forward Kernel
+      GenericTensorAccessorW input_accessor_gpu =
+          create_transformed_accessor_w<float, float>(
+              input_shape, gpu_allocator, transform, false);
+      GenericTensorAccessorW output_accessor_gpu =
+          gpu_allocator.allocate_tensor(output_shape);
+
+      Kernels::Reverse::forward_kernel(managed_stream.raw_stream(),
+                                       input_accessor_gpu.get_float_ptr(),
+                                       output_accessor_gpu.get_float_ptr(),
+                                       num_out_blks,
+                                       reverse_dim_size,
+                                       in_blk_size,
+                                       input_accessor_gpu.shape.num_elements());
+
+      std::vector<float> result_data_gpu = load_accessor_data<DataType::FLOAT>(
+          read_only_accessor_from_write_accessor(output_accessor_gpu), true);
+
+      // Run CPU Cast Forward Kernel
+      GenericTensorAccessorW input_accessor_cpu =
+          create_transformed_accessor_w<float, float>(
+              input_shape, cpu_allocator, transform, true);
+      GenericTensorAccessorW output_accessor_cpu =
+          cpu_allocator.allocate_tensor(output_shape);
+
+      Kernels::Reverse::CPU::forward_kernel(
+          input_accessor_cpu.get_float_ptr(),
+          output_accessor_cpu.get_float_ptr(),
+          num_out_blks,
+          reverse_dim_size,
+          in_blk_size,
+          input_accessor_cpu.shape.num_elements());
+
+      std::vector<float> result_data_cpu = load_accessor_data<DataType::FLOAT>(
+          read_only_accessor_from_write_accessor(output_accessor_cpu), false);
+
+      CHECK(result_data_gpu == result_data_cpu);
+    }
+
+    SUBCASE("backward_kernel") {
+      // Run GPU Cast Backward Kernel
+      GenericTensorAccessorW output_grad_accessor_gpu =
+          create_random_filled_accessor_w(output_shape, gpu_allocator);
+      GenericTensorAccessorW input_grad_accessor_gpu =
+          gpu_allocator.allocate_tensor(input_shape);
+
+      Kernels::Reverse::backward_kernel(
+          managed_stream.raw_stream(),
+          output_grad_accessor_gpu.get_float_ptr(),
+          input_grad_accessor_gpu.get_float_ptr(),
+          num_out_blks,
+          reverse_dim_size,
+          in_blk_size,
+          input_grad_accessor_gpu.shape.num_elements());
+
+      std::vector<float> result_data_gpu = load_accessor_data<DataType::FLOAT>(
+          read_only_accessor_from_write_accessor(input_grad_accessor_gpu),
+          true);
+
+      // Run CPU Cast Backward Kernel
+      GenericTensorAccessorW output_grad_accessor_cpu =
+          copy_tensor_between_memories<DataType::FLOAT>(
+              read_only_accessor_from_write_accessor(output_grad_accessor_gpu),
+              output_shape,
+              cpu_allocator);
+      GenericTensorAccessorW input_grad_accessor_cpu =
+          cpu_allocator.allocate_tensor(input_shape);
+
+      Kernels::Reverse::CPU::backward_kernel(
+          output_grad_accessor_cpu.get_float_ptr(),
+          input_grad_accessor_cpu.get_float_ptr(),
+          num_out_blks,
+          reverse_dim_size,
+          in_blk_size,
+          input_grad_accessor_cpu.shape.num_elements());
+
+      std::vector<float> result_data_cpu = load_accessor_data<DataType::FLOAT>(
+          read_only_accessor_from_write_accessor(input_grad_accessor_cpu),
+          false);
+
+      CHECK(result_data_gpu == result_data_cpu);
+    }
+  }
 }
